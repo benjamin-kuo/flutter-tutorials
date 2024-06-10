4aeced3bee85d853f0bde61f7288c77a7416e787
BK <benjamin@wonder4studio.com>
2024-06-10 22:53:20 +0800

    feat: gestures

---
+++ lib/flutter_08_cookbook_gestures/flutter_08_01_touch_ripples.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+//點按漣漪效果
+//https://docs.flutter.dev/cookbook/gestures/ripples
+//https://docs.flutter.cn/cookbook/gestures/ripples/
+//Create a widget that supports tap.
+//Wrap it in an InkWell widget to manage tap callbacks and ripple animations.
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'InkWell Demo';
+
+    return const MaterialApp(
+      title: title,
+      home: MyHomePage(title: title),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: MyButton(),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // The InkWell wraps the custom flat button widget.
+    return InkWell(
+      // When the user taps the button, show a snackbar.
+      onTap: () {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Tap'),
+        ));
+      },
+      child: const Padding(
+        padding: EdgeInsets.all(12),
+        child: Text('Flat Button'),
+      ),
+    );
+  }
+}
---
+++ lib/flutter_08_cookbook_gestures/flutter_08_03_swipe_to_dismiss.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+//滑動清除效果
+//https://docs.flutter.dev/cookbook/gestures/dismissible
+//https://docs.flutter.cn/cookbook/gestures/dismissible/
+//
+//1. Create a list of items
+//
+//Create a data source
+//Convert the data source into a list
+//
+//2. Wrap each item in a Dismissible widget
+//3. Provide "leave behind" indicators
+
+void main() {
+  runApp(const MyApp());
+}
+
+// MyApp is a StatefulWidget. This allows updating the state of the widget when an item is removed.
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  final items = List<String>.generate(20, (i) => 'Item ${i + 1}');
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Dismissing Items';
+
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return Dismissible(
+              // Each Dismissible must contain a Key. Keys allow Flutter to uniquely identify widgets.
+              key: Key(item),
+              // Provide a function that tells the app what to do after an item has been swiped away.
+              onDismissed: (direction) {
+                // Remove the item from the data source.
+                setState(() {
+                  items.removeAt(index);
+                });
+
+                // Then show a snackbar.
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('$item dismissed')));
+              },
+              // Show a red background as the item is swiped away.
+              background: Container(color: Colors.red),
+              child: ListTile(
+                title: Text(item),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}

01ebc333a490a9b4f3fee12f6c2d0af80314e42a
BK <benjamin@wonder4studio.com>
2024-06-12 08:22:21 +0800

    feat: Navigation

---
+++ lib/flutter_11_cookbook_navigation/flutter_11_02_navigation_basics.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+
+// https://docs.flutter.dev/cookbook/navigation/navigation-basics
+//
+// 1. Create two routes
+// 2. Navigate to the second route using Navigator.push()
+// 3. Return to the first route using Navigator.pop()
+
+void main() {
+  runApp(const CupertinoApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('First Route'),
+      ),
+      child: Center(
+        child: CupertinoButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              CupertinoPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Second Route'),
+      ),
+      child: Center(
+        child: CupertinoButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
